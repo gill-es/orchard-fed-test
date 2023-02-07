6a2f7425a8fedb6531d0e50f0c6c71f7da31285f
Jill Espejo <jill@keepspace.com>
2023-02-08 06:01:09 +0800

    Project setup

---
+++ src/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: "eslint:recommended",
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    quotes: ["error", "double"],
+    "no-console": "warn",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { vars: "all", args: "after-used", ignoreRestSiblings: false },
+    ],
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    "no-empty": "warn",
+  },
+};
---
+++ src/assets/images/index.ts
@@ -0,0 +1,31 @@
+import CP1Image01 from "./component-01/image-01.jpg";
+import CP1Image01Retina from "./component-01/image-01@2x.jpg";
+import CP1Image02 from "./component-01/image-02.jpg";
+import CP1Image02Retina from "./component-01/image-02@2x.jpg";
+import CP1Image03 from "./component-01/image-03.jpg";
+import CP1Image03Retina from "./component-01/image-03@2x.jpg";
+
+import CP2Image01 from "./component-02/image-01.jpg";
+import CP2Image01Retina from "./component-02/image-01@2x.jpg";
+import CP2Image02 from "./component-02/image-02.jpg";
+import CP2Image02Retina from "./component-02/image-02@2x.jpg";
+import CP2Image03 from "./component-02/image-03.jpg";
+import CP2Image03Retina from "./component-02/image-03@2x.jpg";
+
+export const LandingPageImages = {
+  CP1Image01,
+  CP1Image01Retina,
+  CP1Image02,
+  CP1Image02Retina,
+  CP1Image03,
+  CP1Image03Retina,
+};
+
+export const LatestPageImages = {
+  CP2Image01,
+  CP2Image01Retina,
+  CP2Image02,
+  CP2Image02Retina,
+  CP2Image03,
+  CP2Image03Retina,
+};
